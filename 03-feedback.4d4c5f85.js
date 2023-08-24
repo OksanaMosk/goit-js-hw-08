eba52e646d64f517dee888583e22d69198256a94
OksanaMosk <130758466+OksanaMosk@users.noreply.github.com>
2023-08-24 14:07:44 +0000

    Deploying to gh-pages from @ OksanaMosk/goit-js-hw-08@df99f665479cd5ed49986e32413086635a4fadbf 🚀

---
+++ 03-feedback.4d4c5f85.js
@@ -0,0 +1,2 @@
+localStorage.setItem("feedback-form-state",JSON.stringify("email, message"));
+//# sourceMappingURL=03-feedback.4d4c5f85.js.map
---
+++ 03-feedback.4f253023.js
@@ -0,0 +1,2 @@
+localStorage.setItem("feedback-form-state",JSON.stringify("email, message"));
+//# sourceMappingURL=03-feedback.4f253023.js.map
