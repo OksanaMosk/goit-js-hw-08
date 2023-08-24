784baf0bc9647058e50f958ad06d89ca65686321
OksanaMosk <oks-m3@ukr.net>
2023-08-25 01:07:54 +0300

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
-localStorage.setItem("feedback-form-state", JSON.stringify("email, message"))
+import throttle from 'lodash.throttle';
+const emailEl = document.querySelector('input');
+const messageEl = document.querySelector('textarea');
+const buttonEl = document.querySelector('button');
+const feedbackFormOutput = document.querySelector('.feedback-form');
+buttonEl.addEventListener('click', () => {
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify([emailEl.value, messageEl.value])
+  );
+});
+
+form.addEventListener('submit', saveMessage);
+
+function saveMessage(evt) {
+  evt.preventDefault();
+  localStorage.setItem(
+    'feedback - form - state',
+    form.elements.messageEl.value
+  );
+  updateOutput();
+  form.reset();
+}
+
+function updateOutput() {
+  feedbackFormOutput.textContent =
+    localStorage.getItem('feedback - form - state') || '';
+}
+
+/*buttonEl('click', throttle(onClear, 500));
+function onClear({ seconds }) {
+  localStorage.setItem('feedback-form-state', seconds);
+}*/
